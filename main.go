ef26a718e5d6564c859e82ffbc1de223f4715213
Jeroen Massar <massar@fsi.io>
2016-02-18 07:52:08 +0100

    Add fake file thus making imports work

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+/* This is just a placeholder so that one can 'go get trident.li/xmpp-go' */
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Example clients are in 'simple' and in 'xmpp-client'")
+}
